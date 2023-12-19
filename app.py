b702f32d596e07dd0f45307398b64c011cec7d31
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2023-12-19 21:25:00 +0700

    feat: header

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, url_for
+from flask.templating import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Welcome to flask application!</h1>'
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/addnewemployee')
+def addnewemployee():
+    return render_template('addnewemployee.html')
+
+@app.route('/singleemployeeprofile')
+def singleemployeeprofile():
+    return render_template('singleemployeeprofile.html')
+
+@app.route('/updateemployee')
+def updateemployee():
+    return render_template('updateemployee.html')
+
+
+def logout():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
